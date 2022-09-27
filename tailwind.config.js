dc206ff454758e2875bfef00c26106757facf8c6
threeosusanto <112604685+threeosusanto@users.noreply.github.com>
2022-09-27 09:49:54 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      color: {
+        primary: "#14b8a6",
+        dark: "#0f172a",
+      },
+    },
+  },
+  plugins: [],
+};
